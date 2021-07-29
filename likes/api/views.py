b2bc82e112f00feb2f423033923e07a5bbe5d448
SiqiHuang18 <siqihuang18@g.ucla.edu>
2021-07-28 22:01:10 -0700

    Install notifications & added to api

--- likes/api/views.py
+++ likes/api/views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from utils.decorators import required_params
-
+from inbox.services import NotificationService
 
 class LikeViewSet(viewsets.GenericViewSet):
     queryset = Like.objects.all()
@@ -28,6 +28,7 @@ class LikeViewSet(viewsets.GenericViewSet):
                 'errors': serializer.errors,
             }, status=status.HTTP_400_BAD_REQUEST)
         instance = serializer.save()
+        NotificationService.send_like_notification(instance)
         return Response(
             LikeSerializer(instance).data,
             status=status.HTTP_201_CREATED,
--- twitter/settings.py
+++ twitter/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'django_filters',
     "rest_framework",
+    "notifications",
     
     #project apps
     "accounts",
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
     'newsfeeds',
     'comments',
     'likes',
+    'inbox',
 ]
 
 REST_FRAMEWORK = {
